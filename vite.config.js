1abbcb06a1e1980b4076238e383e5f913b584c70
ekrambd <55123967+ekrambd@users.noreply.github.com>
2025-04-22 20:42:33 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allow external access
+    port: 5173,        // Default port or change it if necessary
+  },
 })
